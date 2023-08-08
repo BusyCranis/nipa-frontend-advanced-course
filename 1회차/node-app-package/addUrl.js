0afc6124f2069783f07832e9ee6e5200abcceb24
cranis <cranis2@naver.com>
2023-08-08 16:52:45 +0900

    wdwdw

--- 1회차/node-app-package/addUrl.js
+++ 1회차/node-app-package/addUrl.js
@@ -2,42 +2,42 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 var morgan = require('morgan')
- 
+
 var logger = morgan('combined')
 const hostname = '127.0.0.1';
 const port = 3000;
- 
+
 const server = http.createServer(function (req, res) {
-  
+
   const pageURL = req.url;
   const pathname = url.parse(pageURL, true).pathname;
 
   logger(req, res, function (err) {
     if (err) return console.log(err)
 
-    if (pathname === "/"){
-      
-    fs.readFile("./public/lotto.html", function(err, data){
-      res.statusCode = 200;
-      res.writeHead(200, { 'Content-Type': 'text/html' })
-      res.end(data)
-    })
- 
-  } else if (pathname === "/style.css"){
-      fs.readFile("./public/style.css", 'utf8', function(err, data){
+    if (pathname === "/") {
+
+      fs.readFile("./public/lotto.html", function (err, data) {
+        res.statusCode = 200;
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(data)
+      })
+
+    } else if (pathname === "/style.css") {
+      fs.readFile("./public/style.css", 'utf8', function (err, data) {
         res.writeHead(200);
         res.write(data);
         res.end();
       })
-  } else if (pathname === "/script.js"){
-    fs.readFile("./public/script.js", 'utf8', function(err, data){
-      console.log(data)
-      console.dir(data)
-      res.writeHead(200);
-      // res.write(data);
-      res.end(data);
-    })
-  }
+    } else if (pathname === "/script.js") {
+      fs.readFile("./public/script.js", 'utf8', function (err, data) {
+        console.log(data)
+        console.dir(data)
+        res.writeHead(200);
+        // res.write(data);
+        res.end(data);
+      })
+    }
 
   })
 })
